9f4d84e4ebc8c9e013b3be1f1c4bdd02f970dd19
atezier <atezier@outlook.fr>
2022-06-10 15:09:06 +0200

    Test class ticketDAO

--- src/test/java/com/parkit/parkingsystem/integration/TicketDaoTest.java
+++ src/test/java/com/parkit/parkingsystem/integration/TicketDaoTest.java
@@ -1,48 +1,81 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
-import com.parkit.parkingsystem.integration.config.WrongDataBaseTestConfig;
+import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.util.InputReaderUtil;
+import junit.framework.Assert;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.sql.SQLException;
+import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
+import static com.parkit.parkingsystem.constants.Fare.PREMIUM_CLIENT;
 
 @ExtendWith(MockitoExtension.class)
 public class TicketDaoTest {
-    private static Ticket ticket = null;
+
+    private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+    private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
     private static DataBasePrepareService dataBasePrepareService;
-
+    private static Ticket ticket;
 
     @Mock
-    private static WrongDataBaseTestConfig wrongDataBaseTestConfig = new WrongDataBaseTestConfig();
+    private static InputReaderUtil inputReaderUtil;
 
     @BeforeAll
-    private static void setUp() throws Exception {
-        ticket = new Ticket();
+    private static void setUp() throws Exception{
+        parkingSpotDAO = new ParkingSpotDAO();
+        parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
         ticketDAO = new TicketDAO();
-        ticketDAO.dataBaseConfig = wrongDataBaseTestConfig;
+        ticketDAO.dataBaseConfig = dataBaseTestConfig;
+        dataBasePrepareService = new DataBasePrepareService();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+        ticket = new Ticket();
+        ticket.setInTime(new Date(System.currentTimeMillis()));
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setVehicleRegNumber("ABCDEF");
+        ticket.setPrice(20);
     }
 
+    @BeforeEach
+    private void setUpPerTest() throws Exception {
+        dataBasePrepareService.clearDataBaseEntries();
+        ticketDAO.saveTicket(ticket);
+    }
 
     @Test
-    public void testSaveTicket() throws SQLException, ClassNotFoundException {
-        when(wrongDataBaseTestConfig.getConnection()).thenThrow(SQLException.class);
-        Exception thrown = assertThrows(
-                SQLException.class,
-                () -> ticketDAO.saveTicket(ticket),
-                "Expected doThing() to throw, but it didn't");
-        assertTrue(thrown.getMessage().contains("Error fetching"));
+    public void testSaveTicket() {
+        Assert.assertNotNull(ticketDAO.getTicket("ABCDEF"));
+    }
 
+    @Test
+    public void testUpdateTicket() {
+        ticket.setOutTime(new Date(System.currentTimeMillis()));
+        Assert.assertEquals(true, ticketDAO.updateTicket(ticket));
+    }
 
+    @Test
+    public void testCheckDiscount(){
+        Assert.assertEquals(false, ticketDAO.checkDiscount("ABCDEF"));
+    }
+
+    @Test
+    public void testApplyDiscount(){
+        for(int i = 1 ; i < PREMIUM_CLIENT ; i++){
+            ticketDAO.saveTicket(ticket);
+        }
+        double oldPrice = ticket.getPrice();
+        ticketDAO.applyDiscount(ticket);
+        Assert.assertEquals(false, (oldPrice == ticket.getPrice()));
     }
 }
